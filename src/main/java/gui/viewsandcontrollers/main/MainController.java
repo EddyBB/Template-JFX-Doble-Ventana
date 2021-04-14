6282fcc5c685540eadda2ff4c252932d7635d427
EddyBB <eddy.gonzalez.chavez.al@iespoligonosur.org>
2021-04-14 21:08:36 +0200

    doble ventana

--- src/main/java/gui/viewsandcontrollers/main/MainController.java
+++ src/main/java/gui/viewsandcontrollers/main/MainController.java
@@ -1,20 +1,42 @@
 package gui.viewsandcontrollers.main;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainController {
 
     @FXML
     private Label label;
+    
+    @FXML
+    private TextField textfield;
 
     public void initialize() {
         // TODO
     }
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Stage dialog = new Stage();
+        
+        dialog.initOwner(((Node)event.getSource()).getScene().getWindow());
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../modal/Modal.fxml"));
+        dialog.setScene(new Scene(root));
+        Label label = (Label) root.getChildrenUnmodifiable().get(0);
+        label.setText(textfield.getText());
+        dialog.show();
     }
+    
 }
